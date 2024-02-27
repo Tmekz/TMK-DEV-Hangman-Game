a3f265cab2fb0385d2c72f41ca8c06e239f012b8
Tarig Mekhazni <tmk-dev@hotmail.com>
2024-02-27 12:31:15 +0100

    working savings guessed letters

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,17 @@
-
+import { useState } from "react";
+import words from "./wordsListDB.json";
 
 const App = () => {
+  // using a word list in DB.js
+  const [wordToGuess, setWordToGuess] = useState(() => {
+    return words[Math.floor(Math.random() * words.length)];
+  });
+
+  // letters guessed by users
+const [guessedLetters, setGuessedLetters] = useState<string[]>([]);
+
+  console.log(wordToGuess);
+
   return <div>Hello</div>;
 };
 
